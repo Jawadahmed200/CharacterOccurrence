551229bc47b66efdea434e97e29a2c65c2a49582
Jawad <Jawad@DESKTOP-FSOTPJI>
2023-09-01 20:26:35 +0100

    Add project files.

---
+++ CharacterOccurrence/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter any string : ");
+var inputText = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(inputText) && !string.IsNullOrWhiteSpace(inputText))
+{
+    var result = FindCharacterOccurance(inputText);
+	if(result != null && result.Count > 0)
+	{
+		foreach (var item in result)
+		{
+			Console.WriteLine($"Occurrence of {item.Key} is : {item.Value}");
+		}
+	}
+}
+else
+{
+    Console.WriteLine("Provided text is not valid.");
+}
+
+
+
+
+ Dictionary<char, int> FindCharacterOccurance(string Text)
+{
+	Dictionary<char,int> characterKeyValuePairs = new Dictionary<char,int>();
+	foreach (char c in Text)
+	{
+		characterKeyValuePairs.TryGetValue(c, out int count);
+		characterKeyValuePairs[c] = count + 1;
+    }
+	return characterKeyValuePairs;
+}
